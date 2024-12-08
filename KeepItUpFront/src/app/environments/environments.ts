dc08eea044e62f439a7fb55f568c95418607ea09
FranInfante <infante.portero@gmail.com>
2024-12-08 12:05:02 +0100

    asdasd

--- KeepItUpFront/src/app/environments/environments.ts
+++ KeepItUpFront/src/app/environments/environments.ts
@@ -1,6 +1,6 @@
-// export const environment = {
-//   production: false,
-//   endpointUrl: 'http://localhost:8080/api/v1/',
-//   base: 'http://localhost:8080'
+export const environment = {
+  production: false,
+  endpointUrl: 'http://localhost:8080/api/v1/',
+  base: 'http://localhost:8080'
   
-// };
+};
--- KeepItUpFront/src/app/shared/routes/user-routes.ts
+++ KeepItUpFront/src/app/shared/routes/user-routes.ts
@@ -1,6 +1,7 @@
-import { environment } from '../../environments/environments.prod'
+import { environment } from '../../environments/environments'
 
-export const USER_API_URL = environment.endpointUrl + 'users';
+// export const USER_API_URL = environment.endpointUrl + 'users';
+export const USER_API_URL = 'http://keepitupapp.com/api/api/v1/users';
 
 
 export const BASE = environment.base;
