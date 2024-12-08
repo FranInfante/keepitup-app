b2584bd4a910af3be182c935e83efea6665bfbc8
FranInfante <infante.portero@gmail.com>
2024-12-08 11:58:12 +0100

    asdasd

--- KeepItUpFront/src/app/environments/environments.ts
+++ KeepItUpFront/src/app/environments/environments.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: true,
+  production: false,
   endpointUrl: 'http://localhost:8080/api/v1/',
-  base: 'http://localhost:8080',
+  base: 'http://localhost:8080'
+  
 };
