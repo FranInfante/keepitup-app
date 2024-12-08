c68118dc57d56404d0b6171e63108747cec42863
FranInfante <infante.portero@gmail.com>
2024-12-08 21:58:43 +0100

    updated date format

--- KeepItUpBack/src/main/java/com/example/keepitup/model/dtos/WeighInsDTO.java
+++ KeepItUpBack/src/main/java/com/example/keepitup/model/dtos/WeighInsDTO.java
@@ -1,8 +1,14 @@
 package com.example.keepitup.model.dtos;
 
-import lombok.*;
-
 import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -13,5 +19,7 @@ public class WeighInsDTO {
     private Integer id;
     private Integer userId;
     private double weight;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
 }
