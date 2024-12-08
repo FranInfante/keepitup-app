d02df65c0f0e5f451c8eb480981f50c82decc2ec
FranInfante <infante.portero@gmail.com>
2024-12-08 11:49:09 +0100

    asda

--- KeepItUpFront/src/app/environments/environments.prod.ts
+++ KeepItUpFront/src/app/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    endpointUrl: 'http://keepitupapp.com/api/api/v1/',
-    base: 'http://keepitupapp.com'
+    endpointUrl: 'http://keepitupapp.com//api/v1/',
+    base: 'http://keepitupapp.com/api'
 };
