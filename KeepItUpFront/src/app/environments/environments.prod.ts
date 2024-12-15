601c6558cafa3d158758b8c5a25f7d8212759e15
FranInfante <infante.portero@gmail.com>
2024-12-15 17:06:58 +0100

    updated

--- KeepItUpFront/src/app/environments/environments.prod.ts
+++ KeepItUpFront/src/app/environments/environments.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
-    endpointUrl: 'http://keepitupapp.com/api/api/v1/',
-    base: 'http://keepitupapp.com'
+    // endpointUrl: 'http://keepitupapp.com/api/api/v1/',
+    // base: 'http://keepitupapp.com'
+    endpointUrl: 'http://54.221.132.43/api/api/v1/',
+    base: 'http://54.221.132.43'
 };
