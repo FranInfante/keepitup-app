271bea46904d61148d68271f06b084fcfa1d0f2e
FranInfante <infante.portero@gmail.com>
2024-12-22 15:04:09 +0100

    updated logout button

--- KeepItUpFront/src/app/modules/+menu/menu.component.ts
+++ KeepItUpFront/src/app/modules/+menu/menu.component.ts
@@ -1,26 +1,30 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LOCATIONS } from '../../shared/constants';
+import { UserService } from '../../shared/service/user.service';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
 })
 export class MenuComponent {
-
   LOCATIONS: typeof LOCATIONS = LOCATIONS;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private userService: UserService) {}
 
   navigateToWeighIns() {
-    this.router.navigate([LOCATIONS.weighins]); 
+    this.router.navigate([LOCATIONS.weighins]);
   }
 
   navigateToWorkouts() {
-    this.router.navigate([LOCATIONS.workouts]); 
+    this.router.navigate([LOCATIONS.workouts]);
   }
 
+  logout() {
+    this.userService.logout();
+    this.router.navigate([LOCATIONS.login]);
+  }
 }
