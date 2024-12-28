023ebd70cf19ea52c99e0a9ce4220d6d403cc5f9
FranInfante <infante.portero@gmail.com>
2024-12-28 22:57:30 +0100

    back to menu buttons and redirection to login

--- KeepItUpFront/src/app/modules/+landing/landing.component.ts
+++ KeepItUpFront/src/app/modules/+landing/landing.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import { ASSET_URLS } from '../../shared/constants';
+import { ASSET_URLS, LOCATIONS } from '../../shared/constants';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrl: './landing.component.css',
 })
 export class LandingComponent {
-  
   bg: string = ASSET_URLS.backgroudnd;
   target: string = ASSET_URLS.target;
   tools: string = ASSET_URLS.tools;
   weight: string = ASSET_URLS.weight;
-
-
+  logworkouts: string = ASSET_URLS.logworkouts;
+  LOCATIONS: typeof LOCATIONS = LOCATIONS;
 }
