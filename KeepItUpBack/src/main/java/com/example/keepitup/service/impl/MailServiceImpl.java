e3d2c44a1f0a1af10435bdde50152d535315e3dc
FranInfante <infante.portero@gmail.com>
2025-01-01 11:05:02 +0100

    environment variables

--- KeepItUpBack/src/main/java/com/example/keepitup/service/impl/MailServiceImpl.java
+++ KeepItUpBack/src/main/java/com/example/keepitup/service/impl/MailServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.keepitup.service.impl;
 
 import com.example.keepitup.service.MailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,26 @@ public class MailServiceImpl implements MailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${smtp.host}")
+    private String smtpHost;
+
+    @Value("${smtp.port}")
+    private int smtpPort;
+
+    @Value("${smtp.user}")
+    private String smtpUser;
+
+    @Value("${smtp.pass}")
+    private String smtpPass;
+
+
     @Override
     public void sendSimpleEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        message.setFrom("appkeepitup@gmail.com");
+        message.setFrom(smtpUser); // Use the configured user as the sender
         mailSender.send(message);
     }
 }
